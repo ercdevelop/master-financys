276fb30092e717e83bbb79a5b2f2c46202f8b7aa
erconceicao <eduardo.rc02@gmail.com>
2021-10-31 16:40:19 -0300

    inclusao entries

---
+++ src/app/pages/entries/shared/entry.models.ts
@@ -0,0 +1,17 @@
+import { Category } from './../../categories/shared/category.model';
+export class Entry{
+
+constructor(
+  public id?:number,
+  public name?:string,
+  public description?:string,
+  public type?:string,
+  public amount?:string,
+  public date?:string,
+  public paid?: boolean,
+  public categoryId?: number,
+  public Category?: Category
+  ) {}
+
+
+}
