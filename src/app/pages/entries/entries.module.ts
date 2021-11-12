8ef666828db4bce565a7106b658f93b730605c96
erconceicao <eduardo.rc02@gmail.com>
2021-11-12 11:53:36 -0300

    correçoes erro

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -5,13 +5,19 @@ import { CommonModule } from '@angular/common';
 import { EntriesRoutingModule } from './entries-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EntryFormComponent } from './entries-form/entry-form.component';
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from 'angular-imask';
+
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
+
   ]
 })
 export class EntriesModule { }
