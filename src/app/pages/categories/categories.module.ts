bf5346381f2c8cc2527214280128ae7f238dca2b
erconceicao <eduardo.rc02@gmail.com>
2021-11-16 23:04:44 -0300

    reestruturacao do projeto

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
+    SharedModule,
     CategoriesRoutingModule
   ]
 })
