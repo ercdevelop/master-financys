6dd8bb620df3c14b5bfcbf16ccdfc88f4190353b
erconceicao <eduardo.rc02@gmail.com>
2021-11-16 22:43:04 -0300

    ajuste projeto

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,23 +1,20 @@
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { EntryFormComponent } from './entries-form/entry-form.component';
 import { CalendarModule } from 'primeng/calendar';
 import { IMaskModule } from 'angular-imask';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
-
   ]
 })
 export class EntriesModule { }
