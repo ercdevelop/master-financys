48218776d574eef3527229952848e9ca0c93ef07
erconceicao <eduardo.rc02@gmail.com>
2021-11-27 10:57:22 -0300

    ajuste exibição button page header

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -7,10 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-    @Input('page-title') pageTitle: string
-    @Input('button-class') buttonClass: string
-    @Input('button-text') buttonText: string
-    @Input('button-link') buttonLink: string
+    @Input('page-title') pageTitle: string;
+    @Input('button-class') buttonClass: string;
+    @Input('show-button') showbutton: boolean = true;
+    @Input('button-text') buttonText: string;
+    @Input('button-link') buttonLink: string;
 
   constructor() { }
 
